547a89f3657cf20fa9b104f49a29a43875a52696
dennis1990224 <dennisluzin224@gmail.com>
2021-06-24 21:38:44 -0700

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import router from './router.js'
+import SocialSharing from 'vue-social-sharing'
+import axios from 'axios'
+import MainPage from './components/page/MainPage'
+
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+require('bootstrap');
+
+// require('./bootstrap');
+
+Vue.prototype.$http = axios;
+Vue.use(SocialSharing);
+
+new Vue({
+  
+  router: router, // routerにはrouter.jsファイルを設定
+  components: {
+    app: MainPage
+  }
+
+ }).$mount('#app')
